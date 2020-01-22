9c9711bdbbca2b99bf2f46dd5fffc34d0cef5590
Max Amling <>
2020-01-22 06:17:30 +0100

    cleaned up metaparameter stuff. new more staight forward meta space description and proper validation.

--- metaparameter/__init__.py
+++ metaparameter/__init__.py
@@ -2,96 +2,89 @@ import numpy as np
 from itertools import product
 import pandas as pd
 from collections import OrderedDict
-from . import gp
+#from . import gaussian_process as gp
 
-gaussian_process = gp.gaussian_process
+#gaussian_process = gp
 
 
 def random(metaparamspace):
     """selects metaparams randomly"""
     params = {}
     for key in metaparamspace:
-        value_list = list(unroll_1d(metaparamspace[key]))
+        value_list = list(_unroll_1d(metaparamspace[key]))
+        if len(value_list) == 0:
+            raise ValueError(f'Meta space dimension "{key}" cannot be empty.')
         params[key] = value_list[np.random.choice(len(value_list))]
     return params
 
-def grid_search(metaparamspace, experience, density=1.0):
-    """selects randomly metaparams, with the least epochs trained with"""
-    keys, grid = unroll(metaparamspace)
-    grid_epochs = []
-    for p in grid:
-        experience_run = experience
-        for i in range(len(keys)):
-            key = keys[i]
-            if key in experience_run.columns:
-                value = p[i]
-                experience_run = experience_run[experience_run[key]==value]
-            else: experience_run = pd.DataFrame()
-        epochs = len(experience_run)
-        grid_epochs.append((p, epochs))
-    epoch_min = min(map(lambda x: x[1], grid_epochs))
-    grid_min = filter(lambda x: x[1] == epoch_min, grid_epochs)
-    grid_min_params = list(map(lambda x: x[0], grid_min))
-    params = grid_min_params[np.random.choice(len(grid_min_params))]
-    return dict(zip(keys, params))
 
-def unroll(metaparamspace):
-    grid_space = {}
+def space1d_list(values, allow_interpolation=False):
+    return {
+        'values': values,
+        'allow_interpolation': allow_interpolation,
+    }
+
+def space1d_linear(minimum, maximum, steps=None, allow_interpolation=True):
+    if steps is None and int(minimum) == minimum and int(maximum) == maximum:
+        minimum = int(minimum)
+        maximum = int(maximum)
+        steps = maximum - minimum + 1
+    return {
+        'map_from_uniform': lambda u: u * (maximum - minimum) + minimum,
+        'map_to_uniform': lambda x: (x - minimum) / (maximum - minimum),
+        'steps': steps,
+        'allow_interpolation': allow_interpolation,
+    }
+
+def space1d_log(minimum, maximum, steps, allow_interpolation=True):
+    log_min = np.log(minimum)
+    log_max = np.log(maximum)
+    round_decimals = 16
+    return {
+        'map_from_uniform': lambda u: round(np.exp(u * (log_max - log_min) + log_min), round_decimals),
+        'map_to_uniform': lambda x: round((np.log(x) - log_min) / (log_max - log_min), round_decimals),
+        'steps': steps,
+        'allow_interpolation': allow_interpolation,
+    }
+
+
+
+def _validate_metaparamspace(metaparamspace):
     for key in metaparamspace:
-        grid_space[key] = unroll_1d(metaparamspace[key])
-    # grid_space_keys = sorted(grid_space.keys())
-    # grid_space_values = [grid_space[key] for key in grid_space_keys]
-    # grid = list(product(*grid_space_values))
-    # return grid_space_keys, grid
-    return grid_space
+        mps = metaparamspace[key]
+        if type(mps) is not dict:
+            raise TypeError(f'Metaparamspace description @key "{key}" must be a dictionary, but was {type(mps)}')
+        if 'values' in mps:
+            if type(mps['values']) is not list:
+                raise ValueError(f'Meta space description @key "{key}": "value" field must be a list, but was type {type(mps["values"])}.')
+            if len(mps["values"]) == 0:
+                raise ValueError(f'Meta space description @key "{key}": "value" field cannot be empty.')
+        if 'steps' not in mps:
+            raise ValueError(f'Meta space description @key "{key}" must have either contain number of "steps" or a list of "values", got {", ".join(mps.keys())}.')
+        if 'map_to_uniform' not in mps:
+            raise ValueError(f'Meta space description @key "{key}" must have either contain a function "map_to_uniform" or a list of "values", got {mps.keys()}.')
+        if 'map_from_uniform' not in mps:
+            raise ValueError(f'Meta space description @key "{key}" must have either contain a function "map_from_uniform" or a list of "values", got {mps.keys()}.')
+    return
 
-
-def unroll_1d(metaparamspace1d):
+def _unroll_1d(metaparamspace1d):
     mps = metaparamspace1d
-    if type(mps) == list:
-        value_list = mps
-    elif type(mps) == dict:
-        if 'value' in mps:
-            value_list = [mps['value']]
-        else:
-            is_int = mps.get('integer', False)
-            transform_from_uniform = mps.get('transform_from_uniform', lambda x: x)
-            transform_to_uniform = mps.get('transform_to_uniform', lambda x: x)
-            min_uniform = transform_to_uniform(mps['min'] if not is_int else np.ceil(mps['min']))
-            max_uniform = transform_to_uniform(mps['max'] if not is_int else np.floor(mps['max']))
-            steps = mps['steps']
-            space_uniform = np.linspace(min_uniform, max_uniform, num=steps)
-            value_list = map(transform_from_uniform, space_uniform)
-            if is_int:
-                # round numbers, clamp to [min, max], cast to int, remove duplicates
-                value_list = _rm_duplicates(map(int,
-                                                map(lambda v: np.clip(v, mps['min'], mps['max']),
-                                                    map(np.round,
-                                                        value_list))))
-    return value_list
+    _validate_metaparamspace({'_unroll_1d': mps})
+    
+    if 'values' in mps:
+        return mps['values']
+    
+    is_int = mps.get('integer', False)
+    steps = mps['steps']
+    map_to_uniform = mps['map_to_uniform']
+    map_from_uniform = mps['map_from_uniform']
 
+    value_list = map(map_from_uniform, np.linspace(0, 1, num=steps))
+    return value_list
+    
 
 def _rm_duplicates(values):
     return list(OrderedDict.fromkeys(values))
-
-
-def space1d_linear(minimum, maximum, steps, integer=False):
-    return {
-        'min': minimum,
-        'max': maximum,
-        'steps': steps,
-        'integer': integer,
-    }
-
-def space1d_log10(minimum, maximum, steps, integer=False):
-    return {
-        'min': minimum,
-        'max': maximum,
-        'steps': steps,
-        'transform_to_uniform': np.log10,
-        'transform_from_uniform': lambda x: np.power(10, x),
-        'integer': integer,
-    }
 
 
 # https://arxiv.org/abs/1605.07079
