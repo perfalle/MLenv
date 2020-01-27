5d155e10543194a281d43d444b1eb60684e62ec2
Max Amling <>
2020-01-27 22:11:27 +0100

    Fixed kernel Allow minimization and maximization of objective

--- metaparameter/gaussian_process.py
+++ metaparameter/gaussian_process.py
@@ -10,8 +10,9 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C, PairwiseKernel, Product, WhiteKernel
 
 PER_SAMPLE_VARIANCE = 0.01 # relative to metric variance
+EXPECTED_IMPROVEMENT_REFERENCE = 1 # relative to metric standard deviation
 
-def fixed_invest_gp(metaparamspace, experience, objective_column, invest_value, invest_type):
+def fixed_invest_gp(metaparamspace, experience, objective_column, invest_value, invest_type, objective_minimum=True):
     # select only relevant columns
     columns = {objective_column, invest_type}
     columns.update(list(metaparamspace.keys()))
@@ -47,31 +48,49 @@ def fixed_invest_gp(metaparamspace, experience, objective_column, invest_value, 
     
 
     # get the data for estimating the best choice of metaparameters
-    X = experience[kardinal_columns].values.tolist()
-    Y = experience[objective_column].values.tolist()
+    X = np.array(experience[kardinal_columns].values.tolist())
+    Y = np.array(experience[objective_column].values.tolist())
+
+    # in case of a minimization problem, flip the sign and consider it as a maximazation problem again
+    if objective_minimum:
+        Y = -Y
+
+    Y = Y - np.min(Y)
 
     assert(len(X) == len(Y))
 
-    # choose the first two meassurements at random
-    if len(X) < 2:
+    # choose the first meassurements at random
+    if len(X) < 1:
         return MLenv.metaparameter.random(metaparamspace)
 
     # preparing the gaussian process
 
     # how smooth the curve will be.
+    # choosing interval length over step count from space definition.
+    length_scales_steps = 1 / np.array([metaparamspace[key]['steps'] for key in kardinal_columns])
     # choosing interval length over sample count as a fixed value.
-    length_scales = 1 / np.array(experience[kardinal_columns].nunique())
+    length_scales_n_samples = 1 / np.array(experience[kardinal_columns].nunique())
+
+    length_scales = np.maximum(length_scales_steps, length_scales_n_samples)
+
     length_scale_bounds = [(ls, ls) for ls in length_scales]
+
+    # geuess the standard deviation to 1 when it cannot be estimated from too less data.
+    std = np.std(Y) if len(Y) >= 2 else 1
+    mean = np.mean(Y)
 
     # scale the kernel with data variance (or possibly a constant times that),
     # so that results are invariant under scaling the metric.
-    var = np.std(Y)**2
+    var = std**2
 
     # set the per sample variance (diagonals of the kernel) dependent of the data variance, again for scaling invariance
     whls = var * PER_SAMPLE_VARIANCE
 
     # build up a gaussian process
-    kernel = RBF(length_scales, length_scale_bounds) * C(var, (var, var)) + (0 if whls<=0 else WhiteKernel(whls, (whls, whls)))
+    kernel = RBF(length_scales, length_scale_bounds) * C(var, (var, var))
+    kernel += C(mean, (mean, mean))
+    kernel += (0 if whls<=0 else WhiteKernel(whls, (whls, whls)))
+    
     gp = GaussianProcessRegressor(kernel=kernel)
     
     # fit the gaussian process
@@ -81,7 +100,7 @@ def fixed_invest_gp(metaparamspace, experience, objective_column, invest_value, 
     # that a run with some metaparameters will be at least somewhat better as a reference value
 
     # The reference used is the maximum value plus one standard deviation over the data values
-    reference = np.max(Y) + np.std(Y)
+    reference = np.max(Y) + std * EXPECTED_IMPROVEMENT_REFERENCE
 
     # evaluate the gp at n points and compare /find the best expected improvement
     n = 200
@@ -97,7 +116,8 @@ def fixed_invest_gp(metaparamspace, experience, objective_column, invest_value, 
     # gp predicts a gaussian over the metric for each sample
     mean, sigma = gp.predict(samples_uniform, return_std=True)
 
-    # the required probability is 1-cdf((r-m)/s)), however cdf is strictly monotonic and has bad numerical properties, and therefor ommited.
+    # the required probability is 1-cdf((r-m)/s)).
+    # However cdf is strictly monotonic, has bad numerical properties and is therefore ommited.
     expected_improvement = (np.array([(reference - m) / s for m, s in zip(mean, sigma)]))
     # Also the "1 - " was ommitted, instead the minumal value is now the optimal chioce
     gp_choice_index = np.argmin(expected_improvement)
@@ -110,21 +130,22 @@ def fixed_invest_gp(metaparamspace, experience, objective_column, invest_value, 
     # every metaparameter should be chosen by now
     assert(metaparams.keys() == metaparamspace.keys())
 
+    print(gp_choice)
+
     # this method can only visualize what's going on, if there is just one kardinal parameter.
-    # _plot(gp, X, Y, samples_uniform, mean, sigma)
+    _plot(gp, X, Y, samples_uniform, mean, sigma, reference)
 
     return metaparams
 
 
 
-def _plot(gp, X, Y, sampled_X, sampled_Y, sampled_sigma):
+def _plot(gp, X, Y, sampled_X, sampled_Y, sampled_sigma, reference):
     x_margin = 0
 
     XX = np.atleast_2d(np.linspace(0-x_margin, 1+x_margin, 1000)).T
 
     YY, sigma = gp.predict(XX, return_std=True)
 
-    reference = np.max(Y) + np.std(Y)
     ex_imp = (np.array([(reference - m) / s for m, s in zip(YY, sigma)]))
     sampled_ex_imp = (np.array([(reference - m) / s for m, s in zip(sampled_Y, sampled_sigma)]))
 
